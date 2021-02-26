19909cd93149b7ba59095c5ee1b4f55d889284d4
Boldeusz <mnpower@poczta.onet.pl>
2021-02-26 13:33:41 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6);
 
-        mcLaren.addGear(1,5.3);
-        mcLaren.addGear(2,10.6);
-        mcLaren.addGear(3,15.9);
+
         mcLaren.operateClutch(true);
         mcLaren.changeGear(1);
         mcLaren.operateClutch(false);
