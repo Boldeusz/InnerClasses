e856de7cdb866534755d08049f58817c34960567
Boldeusz <mnpower@poczta.onet.pl>
2021-02-26 13:43:38 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,51 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
+
     public static void main(String[] args) {
-        Gearbox mcLaren = new Gearbox(6);
+
+        class ClickListener implements Button.OnClickListener{
+            public ClickListener() {
+                System.out.println("I have been attached");
+            }
+
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        }
 
 
-        mcLaren.operateClutch(true);
-        mcLaren.changeGear(1);
-        mcLaren.operateClutch(false);
 
-        System.out.println(mcLaren.wheelSpeed(1000));
-        mcLaren.changeGear(2);
-        System.out.println(mcLaren.wheelSpeed(3000));
-        mcLaren.operateClutch(true);
-        mcLaren.changeGear(3);
-        mcLaren.operateClutch(false);
-        System.out.println(mcLaren.wheelSpeed(6000));
+
+
+
+
+
+
+
+
+
+
+
+//        Gearbox mcLaren = new Gearbox(6);
+//
+//
+//        mcLaren.operateClutch(true);
+//        mcLaren.changeGear(1);
+//        mcLaren.operateClutch(false);
+//
+//        System.out.println(mcLaren.wheelSpeed(1000));
+//        mcLaren.changeGear(2);
+//        System.out.println(mcLaren.wheelSpeed(3000));
+//        mcLaren.operateClutch(true);
+//        mcLaren.changeGear(3);
+//        mcLaren.operateClutch(false);
+//        System.out.println(mcLaren.wheelSpeed(6000));
     }
 }
