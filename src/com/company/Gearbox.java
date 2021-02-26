21e1bbacb1f3d3334d447fac25d29afca12e89d5
Boldeusz <mnpower@poczta.onet.pl>
2021-02-26 13:29:17 +0100

    update

--- src/com/company/Gearbox.java
+++ src/com/company/Gearbox.java
@@ -22,6 +22,25 @@ public class Gearbox {
             this.gears.add(new Gear(number,ratio));
         }
     }
+    public void changeGear(int newGear){
+        if ((newGear >= 0) && (newGear < this.gears.size()) && this.clutchIsIn){
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected.");
+        }else {
+            System.out.println("Grind!");
+            this.currentGear = 0;
+        }
+    }
+    public double wheelSpeed(int revs){
+        if (clutchIsIn){
+            System.out.println("Scream!!!!");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
+    }
+
+
+
     private class Gear{
         private int gearNumber;
         private double ratio;
@@ -30,6 +49,11 @@ public class Gearbox {
             this.gearNumber = gearNumber;
             this.ratio = ratio;
         }
+
+        public double getRatio() {
+            return ratio;
+        }
+
         public double driveSpeed(int revs){
             return revs * (this.ratio);
         }
